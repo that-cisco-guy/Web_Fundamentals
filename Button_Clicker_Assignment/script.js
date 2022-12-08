44cc98d9e45f721bc0b4ae993573ef90ccd91cf8
jedifurby <jerryleeandersonjr@gmail.com>
2022-12-08 11:22:59 -0500

    first commit

---
+++ Button_Clicker_Assignment/script.js
@@ -0,0 +1,11 @@
+// Login Button
+function logout(element) {
+    if(element.innerText == "Login")
+        element.innerText = "Logout";
+        else(element.innerText = "Login")
+}
+// Add Def Button
+function hide(element) {
+    element.remove();
+}
+
---
+++ video-preview/script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+function playvid(element) {
+    console.log(element)
+    element.play()
+}
+
+function pausevid(element) {
+    console.log(element)
+    element.pause()
+}
