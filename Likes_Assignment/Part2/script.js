57c5950146204154a2e5611c51ff28719d6d1060
jedifurby <jerryleeandersonjr@gmail.com>
2022-12-10 17:52:18 -0500

    likes assignment 2 update

--- Likes_Assignment/Part2/script.js
+++ Likes_Assignment/Part2/script.js
@@ -1,12 +1,27 @@
 // like button logic
 
-var likeCount = 0;
-var likeCountElement = document.querySelector("#likes");
-
+var likeCount1 = 0;
+var likeCount2 = 0;
+var likeCount3 = 0;
+var likeCountElement1 = document.querySelector("#likes1");
+var likeCountElement2 = document.querySelector("#likes2");
+var likeCountElement3 = document.querySelector("#likes3");
 // console.log(likeCountElement);
 
-function addLike(){
-    likeCount++;
-    likeCountElement.innerText = likeCount + "like(s)"
-    // console.log(likeCount);
+function addLike1(){
+    likeCount1++;
+    likeCountElement1.innerText = likeCount1 + "like(s)"
+    // console.log(likeCount1);
+}
+
+function addLike2(){
+    likeCount2++;
+    likeCountElement2.innerText = likeCount2 + "like(s)"
+    // console.log(likeCount2);
+}
+
+function addLike3(){
+    likeCount3++;
+    likeCountElement3.innerText = likeCount3 + "like(s)"
+    // console.log(likeCount3);
 }
